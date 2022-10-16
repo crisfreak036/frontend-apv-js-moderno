41528a8c19178cc1a45eddbc47bde18cfbde80bf
crisfreak <cristopher.sotog@utem.cl>
2022-10-16 20:07:22 -0300

    [add] tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css' // hoja de estilos principal que se aplica a todos los componentes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
