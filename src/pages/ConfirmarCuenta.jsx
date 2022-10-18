854fc5f3d7a5fe5e3537e2b8d0e6df2126214d36
crisfreak <cristopher.sotog@utem.cl>
2022-10-17 23:44:23 -0300

    [add] los componentes correspondientes a las distintas rutas publicad definidad en app.jsx

---
+++ src/pages/ConfirmarCuenta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConfirmarCuenta() {
+  return (
+    <>
+    <h1>Desde confirmar Cuenta</h1>
+    </>
+  )
+}
+
+export default ConfirmarCuenta
---
+++ src/pages/OlvidePassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OlvidePassword() {
+  return (
+    <>
+    <h1>Desde Olvide Password</h1>
+    </>
+  )
+}
+
+export default OlvidePassword
