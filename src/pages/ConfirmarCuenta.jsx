a28770b7f619a4318e75fad1cb69be68de5fd875
crisfreak <cristopher.sotog@utem.cl>
2022-10-23 00:40:08 -0300

    [add] Contenido y lógica de confirmación de usuario

--- src/pages/ConfirmarCuenta.jsx
+++ src/pages/ConfirmarCuenta.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Alerta } from '../components/Alerta'
 
-function ConfirmarCuenta() {
+
+const ConfirmarCuenta = () => {
+  const debeEjecutarse = useRef(true) // Inicialización de la referencia de ejecución en useEffect
+
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+  const [cargando, setCargando] = useState(true)
+  const [alerta, setAlerta] = useState({})
+
+  const params = useParams()
+  const { id } = params
+
+  useEffect(() => {
+    const confirmacionCeunta = async () => {
+      try {
+        const url = `${import.meta.env.VITE_API_URL_DEV}/veterinarios/confirmar/${id}`
+        const { data } = await axios.get(url)
+        setCuentaConfirmada(true)
+        setAlerta({msg: data.message, error: false})
+
+        console.log(data);
+      } catch (error) {
+        setAlerta({msg: error.response.data.message, error: true})
+      }
+
+      setCargando(false)
+    }
+
+    /*
+    En react 18, useEffect se ejecuta 2 veces cuando se utiliza strictmode por lo cual 
+    se recomienda mantener una referencia que le diga a react si ejecutar o no la función del useEffect
+    */
+    if (debeEjecutarse.current) {
+      debeEjecutarse.current = false // Se cambia el valor a false
+      confirmacionCeunta()
+    }
+  }, [])
+
   return (
     <>
-    <h1>Desde confirmar Cuenta</h1>
+      <div>
+        <h1 className='text-indigo-600 font-black text-6xl'>
+          Confirma tu Cuenta y Comienza a Administrar 
+          <span className='text-black'> tus Pacientes</span>
+        </h1>
+      </div>
+
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+        {
+          cargando && (
+            <h1>CARGANDO...</h1>
+          )
+        }
+        {
+          !cargando && <Alerta
+          alerta={alerta}
+        />
+        }
+        {
+          cuentaConfirmada && (
+            <Link 
+                to="/"
+                className='block text-center my-5 text-gray-500 hover:text-gray-900'
+              >
+                Inicia Sesión Ingresando Aquí
+            </Link>
+          )
+        }
+      </div>
     </>
   )
 }
