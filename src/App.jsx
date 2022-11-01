dd935e15fbb8d145965f6e51ba2fd65548eeca2b
crisfreak <cristopher.sotog@utem.cl>
2022-11-01 17:31:05 -0300

    [add] AuthProvider al routin de la app

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import ConfirmarCuenta from './pages/ConfirmarCuenta'
 import OlvidePassword from './pages/OlvidePassword'
 import NuevoPassword from './pages/NuevoPassword'
 
+import { AuthProvider } from './context/AuthProvider'
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout/>}>
-          <Route index element={<Login/>}/> 
-          <Route path='registrar' element={<Registrar/>}/>
-          <Route path='olvide-password' element={<OlvidePassword/>}/>
-          <Route path='olvide-password/:id' element={<NuevoPassword/>}/>
-          <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout/>}>
+            <Route index element={<Login/>}/> 
+            <Route path='registrar' element={<Registrar/>}/>
+            <Route path='olvide-password' element={<OlvidePassword/>}/>
+            <Route path='olvide-password/:id' element={<NuevoPassword/>}/>
+            <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta/>}/>
+          </Route>
+        </Routes>
+        </AuthProvider>
     </BrowserRouter>
   )
 }
