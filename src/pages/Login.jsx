efaf15d24125e82d2989c9f28653d56da0ae45dd
crisfreak <cristopher.sotog@utem.cl>
2022-11-01 17:32:05 -0300

    [add] hook personalizado useAuth a la página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const Login = () => {
+  const { auth } = useAuth()
   return (
     <>
       <div>
