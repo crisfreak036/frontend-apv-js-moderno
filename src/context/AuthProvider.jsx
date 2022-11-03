3bb404c6fcf05fad67a693f8b5425633cd08f753
crisfreak <cristopher.sotog@utem.cl>
2022-11-02 23:17:44 -0300

    [add] logica de autenticación al proveedor de autenticación

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, createContext } from "react"
+import { useState, useEffect, useRef,createContext } from "react"
+import clienteAxios from "../config/axios";
 
 const Authcontext = createContext(); // Hace referencia al contexto del provider (contexto de autorización)
 
@@ -6,6 +7,38 @@ const AuthProvider = ({children}) => {
     // Se define el state que estará disponible localmente
     // En esta sección se pueden definir otros elementos además de estados
     const [ auth, setAuth ] = useState({});
+
+    const debeEjecutarse = useRef(true)
+
+    useEffect(() => {
+        const autenticarUsuario = async () => {
+            const apvToken = localStorage.getItem('apv_token')
+
+            if (!apvToken) return
+            
+            // Configuración del header de la petición a Axios
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${apvToken}`
+                }
+            }
+
+            try {
+                const { data } = await clienteAxios.get('/veterinarios/perfil', config)
+                setAuth(data.user)
+            } catch (error) {
+                console.log(error.response.data.message);
+                setAuth({})
+            }
+        }
+    
+        if (debeEjecutarse.current) {
+          debeEjecutarse.current = false
+          autenticarUsuario()
+        }
+    }, [])
+
     return(
         // Desde al provider es donde nacen los datos
         <Authcontext.Provider
