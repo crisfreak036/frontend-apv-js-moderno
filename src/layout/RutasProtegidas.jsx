20a541de874b9dcd0485cb7b961f8bc4aada6205
crisfreak <cristopher.sotog@utem.cl>
2022-11-04 15:41:58 -0300

    [add] componentes Header y Footer al layout de las rutas protegidas

--- src/layout/RutasProtegidas.jsx
+++ src/layout/RutasProtegidas.jsx
@@ -3,18 +3,24 @@ import { Outlet, Navigate } from 'react-router-dom'
 
 import useAuth from '../hooks/useAuth'
 
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 const RutasProtegidas = () => {
     const { auth, cargando } = useAuth() 
     if (cargando) return <h1>Cargando...</h1>
     return (
         <>
-        <main>
-            <h1>Este es el Layout de las rutas protegidas</h1>
-            {
-                // En el caso de que auth contenga el id, entonces muestra la vista, de lo contrario envia al login
-                auth?._id ? <Outlet/> : <Navigate to="/"/>
-            }
-        </main>
+        <Header/>
+        {
+            // En el caso de que auth contenga el id, entonces muestra la vista, de lo contrario envia al login
+            auth?._id ? (
+                <main className='container mx-auto mt-10'>
+                    <Outlet/>
+                </main>
+                ) : <Navigate to="/"/>
+        }
+        <Footer/>
         </>
 
     )
