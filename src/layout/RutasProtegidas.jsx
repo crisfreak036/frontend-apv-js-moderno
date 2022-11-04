78dcaafa801a9acfa68c1b29b9d3a6c23f13de43
crisfreak <cristopher.sotog@utem.cl>
2022-11-03 21:50:44 -0300

    [add] protección de rutas según estados globales al layout de las rutas protegidas

--- src/layout/RutasProtegidas.jsx
+++ src/layout/RutasProtegidas.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 
-function RutasProtegidas() {
-  return (
-    <>
-      <main>
-        <h1>Este es el Layout de las rutas protegidas</h1>
-        <Outlet/>
-      </main>
-    </>
+import useAuth from '../hooks/useAuth'
 
-  )
+const RutasProtegidas = () => {
+    const { auth, cargando } = useAuth() 
+    if (cargando) return <h1>Cargando...</h1>
+    return (
+        <>
+        <main>
+            <h1>Este es el Layout de las rutas protegidas</h1>
+            {
+                // En el caso de que auth contenga el id, entonces muestra la vista, de lo contrario envia al login
+                auth?._id ? <Outlet/> : <Navigate to="/"/>
+            }
+        </main>
+        </>
+
+    )
 }
 
 export default RutasProtegidas
