659e8492ce34a64311b17288955acba745dae9cc
crisfreak <cristopher.sotog@utem.cl>
2022-11-05 02:54:31 -0300

    [add] seteo del estado auth con un atributo temporal llamado login

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Alerta } from '../../components/Alerta'
 import clienteAxios from '../../config/axios'
+import useAuth from '../../hooks/useAuth'
 
 const Login = () => {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
   const [ alerta, setAlerta ] = useState({})
+  const { setAuth } = useAuth()
 
   const navigate = useNavigate()
 
@@ -24,8 +26,9 @@ const Login = () => {
     try {
       const { data } =  await clienteAxios.post('/veterinarios/login', { email, password })
       const { data: { jwt } } = data
-
+      console.log(data)
       localStorage.setItem('apv_token', jwt) // Se almacena el token en localStorage
+      setAuth({login: true})
       
       setEmail('')
       setPassword('')
