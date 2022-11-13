66681ee8363329287911083d8c3b852af896dc30
crisfreak <cristopher.sotog@utem.cl>
2022-11-12 22:11:53 -0300

    [add] Proveedor de contexto de pacientes a la App

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,29 @@ import NuevoPassword from './pages/public/NuevoPassword'
 import AdministrarPacientes from './pages/private/AdministrarPacientes'
 
 import { AuthProvider } from './context/AuthProvider'
+import { PacientesProvider } from './context/PacientesProvider'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/' element={<AuthLayout/>}>
-            <Route index element={<Login/>}/> 
-            <Route path='registrar' element={<Registrar/>}/>
-            <Route path='olvide-password' element={<OlvidePassword/>}/>
-            <Route path='olvide-password/:id' element={<NuevoPassword/>}/>
-            <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta/>}/>
-          </Route>
-        </Routes>
+        <PacientesProvider>
+          <Routes>
+            <Route path='/' element={<AuthLayout/>}>
+              <Route index element={<Login/>}/> 
+              <Route path='registrar' element={<Registrar/>}/>
+              <Route path='olvide-password' element={<OlvidePassword/>}/>
+              <Route path='olvide-password/:id' element={<NuevoPassword/>}/>
+              <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta/>}/>
+            </Route>
+          </Routes>
 
-        <Routes>
-          <Route path='/admin' element={<RutasProtegidas/>}>
-            <Route index element={<AdministrarPacientes/>}/>
-          </Route>
-        </Routes>
+          <Routes>
+            <Route path='/admin' element={<RutasProtegidas/>}>
+              <Route index element={<AdministrarPacientes/>}/>
+            </Route>
+          </Routes>
+          </PacientesProvider>
         </AuthProvider>
     </BrowserRouter>
   )
