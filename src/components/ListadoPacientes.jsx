92dbad55d7fcc088793d8fd625d02f902d2fe666
crisfreak <cristopher.sotog@utem.cl>
2022-11-18 21:23:44 -0300

    [add] contenido base para el listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import usePacientes from '../hooks/usePacientes'
 
 const ListadoPacientes = () => {
+    const { pacientes } = usePacientes()
+
     return (
-        <div>ListadoPacientes</div>
+        <>
+            { pacientes.length > 0 ? 
+                (
+                    <>
+                        <h2 className='font-black text-3xl text-center'>
+                            Listado de Pacientes
+                        </h2>
+                        <p className='text-xl mt-5 mb-10 text-center'>
+                            Administra tus {''}
+                            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+                        </p>
+                    </>
+                ) : 
+                (
+                    <>
+                        <h2 className='font-black text-3xl text-center'>
+                            No Hay Pacientes
+                        </h2>
+                        <p className='text-xl mt-5 mb-10 text-center'>
+                            Comienza agregando pacientes {''}
+                            <span className='text-indigo-600 font-bold'>para verlos en este lugar</span>
+                        </p>
+                    </>
+                ) 
+            }
+        </>
     )
 }
 
